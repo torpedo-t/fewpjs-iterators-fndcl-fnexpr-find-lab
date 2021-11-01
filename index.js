7054051647ceee9ba3738ad9bca33ee751780d8d
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-11-01 15:29:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+// write a function called superbowlWin() that receives 1 argument, an array of js objects
+// each object has two properties year and result
+// use find() to test each object to see if the result is "w"
+// return element.year when the element.result = "w" || element.result = "W"
+function superbowlWin(record) {
+    // code to be executed  
+    let winner = record.find(element => element.result === "W");
+    return winner ? winner.year : undefined
+    // console.log("i am hereeeee", winner)
+  }
